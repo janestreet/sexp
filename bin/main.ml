edda62306c0fd910cea117df6746344c4b953253
Xavier Clerc <xclerc@janestreet.com>
2018-10-23 09:40:13 +0100

    v0.11.120.06+181

--- bin/main.ml
+++ bin/main.ml
@@ -5,21 +5,21 @@ let summary = "the s-expression toolkit"
 let command =
   Command.group
     ~summary
-    [ "get", Main_get.command
-    ; "query", Main_query.command
+    [ "assemble", Main_parts.assemble_command
+    ; "change", Main_change.command
+    ; "flatten", Main_parts.flatten_command
+    ; "get", Main_get.command
+    ; "multi-select", Main_select.multi_command
+    ; "of-xml", Main_of_xml.command
     ; "pp", Main_pp.command
     ; "print", Main_print.command
-    ; "validate", Main_validate.command
-    ; "sexpify", Main_sexpify.command
-    ; "change", Main_change.command
-    ; "to-csv", Main_to_csv.command
-    ; "of-xml", Main_of_xml.command
-    ; "flatten", Main_parts.flatten_command
-    ; "assemble", Main_parts.assemble_command
-    ; "select", Main_select.command
-    ; "multi-select", Main_select.multi_command
-    ; "restructure", Main_restructure.command
+    ; "query", Main_query.command
     ; "resolve-macros", Main_resolve_macros.command
+    ; "restructure", Main_restructure.command
+    ; "select", Main_select.command
+    ; "sexpify", Main_sexpify.command
+    ; "to-csv", Main_to_csv.command
+    ; "validate", Main_validate.command
     ]
 ;;
 
