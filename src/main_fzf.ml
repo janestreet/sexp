05d436d6c541fd515919d9908768d98b69af04a1
public-release <as-public-release@janestreet.com>
2025-05-09 21:16:34 +0100

    v0.18~preview.130.31+242

--- src/main_fzf.ml
+++ src/main_fzf.ml
@@ -1,14 +1,30 @@
-open Core
+open! Core
 open Async
 
 let main ~filename =
-  Sys.command
-    ("cat "
-     ^ Sys.quote filename
-     ^ {| | fzf --multi --no-sort --reverse --exact --ansi --preview-window=down:70% \
-        --preview "printf '%s' {} | sexp pp -color -i" |}
-    )
-  >>= exit
+  let fzf =
+    let fzf_binary = "fzf" in
+    let fzf_arguments =
+      [ "--multi"
+      ; "--track"
+      ; "--no-sort"
+      ; "--reverse"
+      ; "--exact"
+      ; "--ansi"
+      ; "--preview-window down:70%"
+      ; "--preview-label-pos bottom"
+      ; "--preview-label 'ctrl+l less | ctrl+alt+c copy | shift+up top | shift+down \
+         bottom'"
+      ; {| --preview "printf '%s' {} | sexp pp -color -i" |}
+      ; {| --bind "ctrl-l:execute(printf '%s' {} | sexp pp -color -i | less -R)" |}
+      ; {| --bind "ctrl-alt-c:execute-silent(printf '%s' {} | sexp pp -i | xclip -selection clipboard)" |}
+      ; {| --bind "shift-up:first" |}
+      ; {| --bind "shift-down:last" |}
+      ]
+    in
+    [%string {| %{fzf_binary} %{String.concat fzf_arguments ~sep:" "} |}]
+  in
+  Sys.command [%string {| cat %{Sys.quote filename} | %{fzf} |}] >>= exit
 ;;
 
 let command =
